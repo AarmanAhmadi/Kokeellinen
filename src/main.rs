efe03a99f27a4f2cf921f34e3f1d989a6f9d2a33
Can't Sleep <Arman.Ahmadi@student.lut.fi>
2025-05-15 14:40:11 +0300

    Ensimmäiset yksikkötestit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    // Lataa kirjastoja yksikkötestien ulkopuolelta.
+    //use super::*;
+
+    #[test]
+    fn test_lisaa() {
+        assert_eq!(1 + 2, 3);
+    }
+
+    #[test]
+    fn test_random_true() {
+        let x = 4;
+        let y = 2 + 2;
+        assert_eq!(x, y);
+    }
+}
